a6047633f2a08d25510ebd35bdeeed34a9d8c9a9
DuongTran <ttduongtran@gmail.com>
2020-04-27 22:25:58 +0700

    hihi

--- index.vr.js
+++ index.vr.js
@@ -6,7 +6,12 @@ export default class demo extends React.Component {
     return (
       <View>
         <App />
+<<<<<<< HEAD
         <p></p>
+=======
+        <p>1</p>
+        <p>2</p>
+>>>>>>> e725450... 2
       </View>
     );
   }
